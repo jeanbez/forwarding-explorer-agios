33d6a70de29c967468887e0a90c4300a987848f3
Francieli Zanon Boito <francielizanon@gmail.com>
2016-11-01 23:35:21 -0200

    finished coding the fastDTW part

--- DTW.c
+++ DTW.c
@@ -1,5 +1,57 @@
+/*
+ * Copyright (c) 2004 Stan Salvador
+ * stansalvador@hotmail.com
+
+ The MIT License (MIT)
+
+Copyright (c) 2004 Stan Salvador
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+ *  Translated from JAVA to C++ by Ramon Nou - Barcelona Supercomputing Center - IOLanes EU Project
+ * Translated to C and adapted by Francieli Zanon Boito
+ */
+
+/* File:	DTW.c
+ * Created: 	November 2016 
+ * License:	GPL version 3
+ * Author:
+ *		Francieli Zanon Boito <francielizanon (at) gmail.com>
+ *
+ * Description:
+ *		This file is part of the AGIOS I/O Scheduling tool.
+ *		It contains the fastDTW method implementation to compare access patterns	
+ *		Further information is available at http://agios.bitbucket.org/
+ *
+ * Contributors:
+ *		Federal University of Rio Grande do Sul (UFRGS)
+ *		INRIA France
+ *
+ *		inspired in Adrien Lebre's aIOLi framework implementation
+ *	
+ *		This program is distributed in the hope that it will be useful,
+ * 		but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * 		MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ */
+
 #include <math.h>
 
+#include "common_functions.h"
 #include "DTW.h"
 
 
@@ -7,21 +59,36 @@ inline struct time_warp_info_t * DTW_getWarpInfoBetween(struct access_pattern_t 
 {
 	return DTW_DynamicTimeWarp(tsI, tsJ);
 } 
-inline struct time_warp_info_t *DTW_getWarpInfoBetween(struct access_pattern_t *tsI, struct access_pattern_t *tsJ, struct search_window_t *window)
+inline struct time_warp_info_t *DTW_getWarpInfoBetween_withWindow(struct access_pattern_t *tsI, struct access_pattern_t *tsJ, struct search_window_t *window)
 {
 	return DTW_constrainedTimeWarp(tsI, tsJ, window);
 }
 
+//allocates and initializes a new memory_resident_matrix_t structure, used by constrained DTW. Returns NULL on error
 inline struct memory_resident_matrix_t *get_new_memory_resident_matrix_t(struct seach_window_t *window)
 {
 	int i;
 	int currentOffset=0;
 
 	struct memory_resident_matrix_t *ret = malloc(sizeof(struct memory_resident_matrix_t));
-	//TODO malloc error
+	if(ret == NULL)
+	{
+		agios_print("PANIC! Could not allocate memory for DTW\n");
+		return NULL;
+	}
 	ret->cellValues = malloc(sizeof(double)*window->size);
+	if(ret->cellValues == NULL)
+	{
+		agios_print("PANIC! Could not allocate memory for DTW\n");
+		return NULL;
+	}
 	ret->cellValues_size=0;
 	ret->colOffsets = malloc(sizeof(int)*window->size);
+	if(ret->colOffsets == NULL)
+	{
+		agios_print("PANIC! Could not allocate memory for DTW\n");
+		return NULL;
+	}
 	ret->colOffsets_size=0;
 
 	for(i = 0; i < window->size; i++)
@@ -34,10 +101,13 @@ inline struct memory_resident_matrix_t *get_new_memory_resident_matrix_t(struct 
 }
 inline void add_to_memory_resident_matrix_t(struct memory_resident_matrix_t *matrix, struct search_window_t *window, int col, int row, double value)
 {
-//	if((row < window->minValues[col]) || (row > window->maxValues[col]))
-	//TODO error
-	//else
-	matrix->cellValues[matrix->colOffsets[col]+row-window->minValues[col]] = value;
+	if((row < window->minValues[col]) || (row > window->maxValues[col]))
+	{
+		agios_print("PANIC! Trying to add a cell to memory resident matrix which is out of the limits given by the window.\n");
+		//now what?
+	}
+	else
+		matrix->cellValues[matrix->colOffsets[col]+row-window->minValues[col]] = value;
 }
 inline double get_from_memory_resident_matrix_t(struct memory_resident_matrix_t *matrix, struct search_window_t *window, int col, int row)
 {
@@ -46,11 +116,28 @@ inline double get_from_memory_resident_matrix_t(struct memory_resident_matrix_t 
 	else
 		return matrix->cellValues[matrix->colOffsets[col]+row - window->minValues[col]];
 }
-
-
+//frees previously allocated memory_resident_matrix_t structure, including its internal lists
+inline void free_memory_resident_matrix_t(struct memory_resident_matrix_t **matrix)
+{
+	if(*matrix)	
+	{
+		if((*matrix)->cellValues)
+			free((*matrix)->cellValues);
+		if((*matrix)->colOffsets)
+			free((*matrix)->colOffsets);
+		free(*matrix);
+	}
+}
 struct time_warp_info_t *DTW_constrainedTimeWarp(struct access_pattern_t *tsI, struct access_pattern_t *tsJ, struct search_window_t *window)
 {
 	unsigned int colu, i, j;
+	double diagCost, leftCost, downCost;
+	struct time_warp_info_t *ret = malloc(sizeof(struct time_warp_info)); 
+	if(ret == NULL)
+	{
+		agios_print("PANIC! Could not allocate memory for DTW\n");
+		return NULL;
+	}
 
 	//     COST MATRIX:
 	//   5|_|_|_|_|_|_|E| E = min Global Cost
@@ -63,11 +150,19 @@ struct time_warp_info_t *DTW_constrainedTimeWarp(struct access_pattern_t *tsI, s
 	//            i
 	//   access is M(i,j)... column-row
 	struct memory_resident_matrix_t *costMatrix = get_new_memory_resident_matrix_t(window);
+	if(costMatrix == NULL)
+	{
+		agios_print("PANIC! Could not apply DTW\n");
+		free_time_warp_info_t(&ret);
+		return NULL;
+	}
 	int maxI = tsI->reqnb-1;
 	int maxJ = tsJ->reqnb-1;
 	
 	//get an iterator that traverses the window cells in the order that the cost matrix is filled (first to last row (1...maxI), bottom to top (1...maxJ)
 	struct search_window_t *matrixIterator = SearchWindow_obtain(window);
+	if(matrixIterator == NULL)
+		return NULL;
 	for(colu = 0; colu < matrixIterator->size; colu++)
 	{
 		//current cell being filled
@@ -76,55 +171,134 @@ struct time_warp_info_t *DTW_constrainedTimeWarp(struct access_pattern_t *tsI, s
 		if((i == 0) && (j == 0)) //bottom left cell (first row and first column)
 			add_to_memory_resident_matrix_t(costMatrix, window, i, j, DTW_euclideanDist(tsI->time_series[0].offset, tsJ->time_series[0].offset));
 		else if (i == 0) //first column
-			add_to_memory_resident_matrix_t(costMatrix, window, i, j, DTW_euclideanDist(tsI->time_series[0].offset, tsJ->time_series[j].offset) + get_from_memory_resident_matrix_t(CostMatrix, window, i, j-1));
+			add_to_memory_resident_matrix_t(costMatrix, window, i, j, DTW_euclideanDist(tsI->time_series[0].offset, tsJ->time_series[j].offset) + get_from_memory_resident_matrix_t(costMatrix, window, i, j-1));
 		else if (j == 0) //first row
-			add_to_memory_resident_matrix_t(costMatrix, window, i, j, DTW_euclideanDist(tsI->time_series[i].offset, tsJ->time_series[0]) + get_from_memory_resident_matrix_t(CostMatrix, window, i-1,j));
+			add_to_memory_resident_matrix_t(costMatrix, window, i, j, DTW_euclideanDist(tsI->time_series[i].offset, tsJ->time_series[0]) + get_from_memory_resident_matrix_t(costMatrix, window, i-1,j));
 		else //not first column or first row		
-			add_to_memory_resident_matrix_t(costMatrix, window, i, j, fmin(
-//HERE
-//TODO continue here
-	
+			add_to_memory_resident_matrix_t(costMatrix, window, i, j, fmin(get_from_memory_resident_matrix_t(costMatrix, window, i-1, j), fmin(get_from_memory_resident_matrix_t(costMatrix, window, i-1,j-1), get_from_memory_resident_matrix_t(costMatrix, window, i,j-1))) + DTW_euclideanDist(tsI->time_series[i].offset, tsJ->time_series[j].offset));
+	} // end for
+	free_search_window_t(&matrixIterator);
+	//minimum cost is at (max i, max j)
+	ret->distance = get_from_memory_resident_matrix_t(costMatrix, window, maxI, maxJ);
 
+	//find the warp path by searching the matrix from the solution at (max i, max j) to the beginning at (0,0). at each step move through the matrix 1 step left, down, or diagonal, whichever has the smallest cost. Favor diagonal moves and moves towards the i==j axis to break ties
+	Initialize_TimeWarp_Path(&ret->path, (maxI + maxJ - 1));
+	i = maxI;
+	j = maxJ;
+	Add_to_TimeWarp_Path(&ret->path, i, j);
+	
+	while((i > 0) || (j > 0))
+	{
+		//find the costs of moving in all three possible directions (left, down, and diagonal (down and left at the same time)
+		if((i > 0)&& (j > 0))
+			diagCost = get_from_memory_resident_matrix_t(costMatrix, window, i-1, j-1);
+		else
+			diagCost = DBL_MAX;
+		if(i > 0)
+			leftCost = get_from_memory_resident_matrix_t(costMatrix, window, i-1,j);
+		else
+			leftCost = DBL_MAX;
+		if(j > 0)
+			downCost = get_from_memory_resident_matrix_t(costMatrix, window, i, j-1);
+		else
+			downCost = DBL_MAX;
+		//determine which direction to move in. Prefer moving diagonally and towards the i==j axis of the matrix if there are ties
+		if((diagCost <=	leftCost) && (diagCost <= downCost))
+		{
+			i--;
+			j--;
+		}
+		else if((leftCost < diagCost) && (leftCost < downCost))
+			i--;
+		else if((downCost < diagCost) && (downCost < leftCost))
+			j--;
+		else if(i <= j) //leftCost == rightCost > diagCost
+			j--;
+		else
+			i--;
+		//add the current step to the warp path
+		Add_to_TimeWarp_Path(&ret->path, i, j);
+	} //end while
+
+
+	//cleanup
+	free_memory_resident_matrix_t(&costMatrix);
+
+	return ret;
 }
 
 //starts a new empty path and allocates memory for the provided maximum size
 inline void Initialize_TimeWarp_Path(struct warp_path_t *new, unsigned long int size)
 {
-	new->tsIindexes = malloc(size);
-	new->tsJindexes = malloc(size);
-	//TODO check for malloc error
+	new->tsIindexes = malloc(sizeof(int)*size);
+	new->tsJindexes = malloc(sizeof(int)*size);
+	if((new->tsIindexes == NULL) || (new->tsJindexes == NULL))
+	{	
+		agios_print("PANIC! Cannot allocate memory for DTW.\n");
+		//now what?
+	}
 	new->index = 0;
+	new->max = size;
 }
 //adds a point (a,b) to the beginning of the path
 inline void Add_to_TimeWarp_Path(struct warp_path_t *path, int a, int b)
-{//TODO should we check for errors if we try adding more stuff than it can fit?
+{
 	int i;
 	path->index++;
-	if(path->index > 1)
+	if(path->index < path->max)
 	{
-		//move everything one position to the right
-		for(i = path->index; i> 0; i++)
+		if(path->index > 1)
 		{
-			path->tsIindexes[i] = path->tsIindexes[i-1];	
-			path->tsJindexes[i] = path->tsJindexes[i-1];
-		}	
+			//move everything one position to the right
+			for(i = path->index; i> 0; i++)
+			{
+				path->tsIindexes[i] = path->tsIindexes[i-1];	
+				path->tsJindexes[i] = path->tsJindexes[i-1];
+			}	
+		}
+		//insert the new point
+		path->tsIindexes[0] = a;
+		path->tsJindexes[0] = b;
 	}
-	//insert the new point
-	path->tsIindexes[0] = a;
-	path->tsJindexes[0] = b;
+	else
+	{
+		agios_print("PANIC! Trying to add a point to a time warp path which is already full\n");
+		//now what?
+	}
 }
 
 struct time_warp_info_t * DTW_DynamicTimeWarp(struct access_pattern_t *tsI, struct access_pattern_t *tsJ)
 {
 	int i,j;
 	struct time_warp_info_t *ret = malloc(sizeof(time_warp_info_t));
+	if(ret == NULL)
+	{
+		agios_print("PANIC! Could not allocate memory for DTW");
+		return NULL;
+	}
 
 	//allocate a cost matrix
 	double **costMatrix;
 	costMatrix = (double **)malloc(sizeof(double *)*(tsI->reqnb+1));
+	if(costMatrix == NULL)
+	{
+		agios_print("PANIC! Could not allocate memory for DTW");
+		free_time_warp_info_t(&ret);
+		return NULL;
+	}
 	for(i = 0; i < tsI->reqnb; i++)
 	{
 		costMatrix[i] = (double *)malloc(sizeof(double)*(tsJ->reqnb+1));
+		if(costMatrix[i] == NULL)
+		{
+			int index;
+			agios_print("PANIC! Could not allocate memory for DTW");
+			free_time_warp_info_t(&ret);
+			for(index = 0; index < i; index++)
+				free(costMatrix[index]);
+			free(costMatrix);
+			return NULL;
+		}
 		for(j=0; j < tsJ->reqnb; j++)
 			costMatrix[i][j] = 0;
 	}
@@ -146,7 +320,7 @@ struct time_warp_info_t * DTW_DynamicTimeWarp(struct access_pattern_t *tsI, stru
 	ret->distance = costMatrix[tsI->reqnb-1][tsJ->reqnb-1];
 
 	//find the warp path by searching the matrix form the solution to at (maxI, maxJ) to the beginning at (0,0). At each step move through the matrix 1 step left, down, or diagonal, whichever has the smallest cost. Favor diagonal moves and moves towards the i==j axis to break ties.
-	Initialize_TimeWarp_Path(&ret->path, sizeof(int)*(tsI->reqnb+tsJ->reqnb-1));
+	Initialize_TimeWarp_Path(&ret->path, (tsI->reqnb+tsJ->reqnb-1));
 	i = tsI->reqnb-1;
 	j = tsJ->reqnb-1;
 	Add_to_TimeWarp_Path(&ret->path, i, j);
@@ -208,14 +382,32 @@ struct access_pattern_t * shrink_time_series(struct access_pattern_t *ts, int sh
 	struct access_pattern_t *ret;
 
 	if(ts->reqnb <= 0)
-		return NULL; //TODO Notify error?
+	{
+		agios_print("PANIC! Trying to shrink an empty access pattern???\n");
+		return NULL; 
+	}
 
 	ret = malloc(sizeof(struct access_pattern_t));
+	if(ret == NULL)
+	{
+		agios_print("PANIC! Could not allocate memory for DTW\n");
+		return NULL;
+	}
 	ret->time_series = malloc(sizeof(struct pattern_tracker_req_info_t)*(shrunkSize+1));
 	ret->aggPtSize = malloc(sizeof(int)*(shrunkSize+1));
-	//TODO check malloc error
-	//if((shrunkSize > ts->reqnb) || (shrunkSize <= 0))
-		//TODO error
+	if((ret->time_series == NULL) || (ret->aggPtSize == NULL))
+	{
+		agios_print("PANIC! Could not allocate memory for DTW\n");
+		free_access_pattern_t(&ret);
+		return NULL;
+	}
+	
+	if((shrunkSize > ts->reqnb) || (shrunkSize <= 0))
+	{
+		agios_print("PANIC! Trying to shrink time series of size %d with non-sensical size %d\n", ts->reqnb, shrunkSize);
+		free_access_pattern_t(&ret);
+		return NULL;
+	}
 	ret->renqb=0;
 	ret->original_size = ts->reqnb;
 
@@ -248,11 +440,21 @@ inline struct search_window_t * new_search_window(int tsIsize, int isJsize)
 {
 	int i;
 	struct search_window_t *ret = malloc(sizeof(struct search_window_t));
+	if(ret == NULL)
+	{
+		agios_print("PANIC! Could not allocate memory for DTW\n");
+		return NULL;
+	}
 	ret->minValues = malloc(sizeof(int)*(tsIsize+1));
+	ret->maxValues = malloc(sizeof(int)*(tsIsize+1));
+	if((ret->minValues == NULL) || (ret->maxValues == NULL))
+	{
+		agios_print("PANIC! Could not allocate memory for DTW\n");
+		free_search_window_t(&ret);
+		return NULL;
+	}
 	for(i=0; i< tsIsize; i++)
 		ret->minValues[i]=-1;
-	ret->maxValues = malloc(sizeof(int)*(tsIsize+1));
-	//TODO check malloc error
 	ret->maxJ_ = tsJsize-1;
 	ret->size = 0;
 	ret->modCount=0;
@@ -301,10 +503,20 @@ inline struct search_window_t *SearchWindow_obtain(struct search_window_t *windo
 	int currentI, currentJ;
 	short int hasMoreElements;
 	struct search_window_t *new = malloc(sizeof(struct search_window_t));
+	if(new == NULL)
+	{
+		agios_print("PANIC! Could not allocate memory for DTW\n");
+		return NULL;
+	}
 	new->minValues = malloc(sizeof(int)*(window->size+1));
-	new->maxVales = malloc(sizeof(int)*(window->size+1));
+	new->maxValues = malloc(sizeof(int)*(window->size+1));
+	if((new->minValues == NULL) || (new->maxValues == NULL))
+	{
+		agios_print("PANIC! Could not allocate memory for DTW\n");
+		free_search_window_t(&new);
+		return NULL;
+	}
 	new->size=0;
-	//TODO check malloc error
 
 	if(window->size > 0)
 		hasMoreElements = 1;
@@ -339,6 +551,8 @@ void SearchWindow_expandSearchWindow(struct search_window_t *window, int radius)
 		//add all cells in the current window to an array, iterating through the window and expanding the window
 		//at the same time it is not possible because the window can't be changed during iteration through the cells
 		struct search_window_t *windowCells = SearchWindow_obtain(window);
+		if(windowCells == NULL)
+			return;
 		for(cell = 0; cell < windowCells->size; cell++)
 		{
 			if((windowCells->minValues[cell] != 0) && (windowCells->maxValues[cell] != window->maxJ_)) //move to upper left is possible
@@ -469,6 +683,10 @@ struct search_window_t *SearchWindow_ExpandedResWindow(struct access_pattern_t *
 	int lastWarpedI = INT_MAX;
 	int lastWarpedJ = INT_MAX;
 	struct search_window_t *ret = new_search_window(tsI->reqnb, tsJ->reqnb);
+	if(ret == NULL)
+	{
+		return NULL;
+	}
 	//for each part of the low-resolution warp path, project that path to the higher resolution by filling in the path's corresponding cells at the higher resolution
 	for(w = 0; w < path->index; w++)
 	{
@@ -537,12 +755,39 @@ struct time_warp_info_t *FastDTW_fastDTW(struct access_pattern_t *tsI, struct ac
 	{
 		struct access_pattern_t *shrunkI = shrink_time_series(tsI, (tsI->reqnb / 2));
 		struct access_pattern_t *shrunkJ = shrink_time_series(isJ, (tsJ->reqnb / 2));
+		if((shrunkI == NULL) || (shrunkJ == NULL))
+		{
+			agios_print("PANIC! Could not shrink time series from access patterns\n");
+			return NULL;
+		}
 
 		//determine the search window that constrains the area of the cost matrix that will be evaluated based on the warp path found at the previous resolution (smaller time series)
-		struct time_warp_info_t *tmp = fastDTW(shrunkI, shrunkJ, searchRadius);
+		struct time_warp_info_t *tmp = FastDTW_fastDTW(shrunkI, shrunkJ, searchRadius);
+		if(tmp == NULL)
+		{
+			free_access_pattern_t(&shrunkI);
+			free_access_pattern_t(&shrunkJ);
+			return NULL;
+		}
 		struct search_window_t *window = SearchWindow_ExpandedResWindow(tsI, tsJ, shrunkI, shrunkJ, &tmp->path, searchRadius);
+		if(window == NULL)
+		{
+			free_access_pattern_t(&shrunkI);
+			free_access_pattern_t(&shrunkJ);
+			free_time_warp_info_t(&tmp);
+			return NULL;
+		}
 		//find the optimal path through this search window constraint
 		struct time_warp_info_t *ret = DTW_getWarpInfoBetween_withWindow(tsI, tsJ, window);
+		if(ret == NULL)
+		{
+			free_access_pattern_t(&shrunkI);
+			free_access_pattern_t(&shrunkJ);
+			free_search_window_t(&window);
+			free_time_warp_info_t(&tmp);
+			return NULL;
+		}
+		
 		//cleanup
 		free_search_window_t(&window);
 		free_time_warp_info_t(&tmp);
@@ -559,8 +804,17 @@ struct time_warp_info_t *FastDTW_getWarpInfoBetween(struct access_pattern_t *tsI
 
 double FastDTW(struct access_pattern_t *A, struct access_pattern_t *B)
 {
+	double ret;
 	struct time_warp_info_t *info = FastDTW_getWarpInfoBetween(A, B, 1);
-	double ret = info->distance;
-	free(info);
+	if(info)
+	{
+		ret = info->distance;
+		free_time_warp_info_t(info);
+	}
+	else
+	{
+		agios_print("PANIC! Could not apply fast DTW");
+		ret = DBL_MAX;
+	}
 	return ret;
 }
--- DTW.h
+++ DTW.h
@@ -1,3 +1,53 @@
+/*
+ * Copyright (c) 2004 Stan Salvador
+ * stansalvador@hotmail.com
+
+ The MIT License (MIT)
+
+Copyright (c) 2004 Stan Salvador
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+ *  Translated from JAVA to C++ by Ramon Nou - Barcelona Supercomputing Center - IOLanes EU Project
+ * Translated to C and adapted by Francieli Zanon Boito
+ */
+
+/* File:	DTW.h
+ * Created: 	November 2016 
+ * License:	GPL version 3
+ * Author:
+ *		Francieli Zanon Boito <francielizanon (at) gmail.com>
+ *
+ * Description:
+ *		This file is part of the AGIOS I/O Scheduling tool.
+ *		It contains the fastDTW method implementation to compare access patterns	
+ *		Further information is available at http://agios.bitbucket.org/
+ *
+ * Contributors:
+ *		Federal University of Rio Grande do Sul (UFRGS)
+ *		INRIA France
+ *
+ *		inspired in Adrien Lebre's aIOLi framework implementation
+ *	
+ *		This program is distributed in the hope that it will be useful,
+ * 		but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * 		MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ */
 #ifndef _DTW_H_
 #define _DTW_H_
 
@@ -9,6 +59,7 @@ struct warp_path_t {
 	int * tsIindexes;
 	int * tsJindexes;
 	int index;
+	int max;
 };
 
 struct time_warp_info_t {
